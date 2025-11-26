1086f4e9f4e85cbcfffbcc1f0d240ba6dcb87128
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-11-26 18:27:33 +0800

    finished admin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { sendMail } from "./email";
 import { ForgotPasswordEmailHTML } from "@/components/email-template/forgot-password";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL!,
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),

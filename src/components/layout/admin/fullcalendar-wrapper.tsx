69cfefa5b7447a03156a16aa9a515828397d0121
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-11-26 16:41:56 +0800

    finished some

--- src/components/layout/admin/fullcalendar-wrapper.tsx
+++ src/components/layout/admin/fullcalendar-wrapper.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-// This wrapper component uses standard imports as per FullCalendar React docs
-// https://fullcalendar.io/docs/react
-// @ts-expect-error - FullCalendar packages may not have type declarations
 import FullCalendar from "@fullcalendar/react";
-// @ts-expect-error - FullCalendar packages may not have type declarations
 import dayGridPlugin from "@fullcalendar/daygrid";
-// @ts-expect-error - FullCalendar packages may not have type declarations
 import interactionPlugin from "@fullcalendar/interaction";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any

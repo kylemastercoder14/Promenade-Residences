f4227264494cd6a3f1a4c2e002964edd4e95c315
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-11-27 20:28:21 +0800

    finished

--- src/lib/routers/logs.ts
+++ src/lib/routers/logs.ts
@@ -5,7 +5,7 @@ import { TRPCError } from "@trpc/server";
 import { ADMIN_FEATURE_ACCESS, hasRequiredRole } from "@/lib/rbac";
 
 const logsProcedure = protectedProcedure.use(({ ctx, next }) => {
-  if (!hasRequiredRole(ctx.auth.user.role, ADMIN_FEATURE_ACCESS.SYSTEM_LOGS)) {
+  if (!hasRequiredRole(ctx.auth.user.role, [...ADMIN_FEATURE_ACCESS.SYSTEM_LOGS])) {
     throw new TRPCError({
       code: "FORBIDDEN",
       message: "You do not have permission to view system logs.",
